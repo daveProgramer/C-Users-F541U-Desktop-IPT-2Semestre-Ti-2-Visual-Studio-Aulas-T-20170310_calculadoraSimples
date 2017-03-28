12b7ed4d3b7bfa31b282ab76ab5d17616f55afe8
F541U <F541U@DESKTOP-27T6CPM>
2017-03-28 12:19:02 +0100

    código associado aos algarismos

---
+++ Caculador-Completa/Caculador-Completa/Controllers/ControlosController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Caculador_Completa.Controllers
+{
+    public class ControlosController : Controller
+    {
+        // GET: Controlos
+        public ActionResult Index(string bt, string visor)
+        {
+            switch (bt) {
+                case "0": 
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    //determinar se n  VISOR só existe um zero
+                    if (visor.Equals("0")) visor = bt;
+                    else visor += bt; // visor = visor + bt;
+                    break;
+            }
+            //entrgar os valores à VIEW
+            ViewBag.Visor = visor;
+
+
+            return View();
+        }
+    }
+}
